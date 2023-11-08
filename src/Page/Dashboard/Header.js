3c90c7e4412427d4449aee9162e19af90066cee8
Aman kohare <amankohare@gmail.com>
2023-11-08 10:07:22 +0530

    done all

--- src/Page/Dashboard/Header.js
+++ src/Page/Dashboard/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Header({ setIsAdding }) {
     return (
         <header>
-            <h1 className='text-center'>Student Management Software</h1>
+            <h1 className='text-center'>Student Management System</h1>
             <div style={{ marginTop: '30px', marginBottom: '18px' }}>
                 <button onClick={() => setIsAdding(true)} className='round-button'>Add Student</button>
             </div>
